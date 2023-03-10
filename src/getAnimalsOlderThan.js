28df52ba2a8beba9fcf401b681a63bb944de2240
Leonardo Martins <leonardogustavo@outlook.com>
2023-03-10 16:33:09 -0300

    Req 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const speciesFound = data.species
+    .find((especie) => (animal.includes(especie.name))).residents;
+  const checkAge = speciesFound
+    .every((idade) => idade.age >= age);
+  return checkAge;
 };
 
+getAnimalsOlderThan('otters', 7);
 module.exports = getAnimalsOlderThan;
