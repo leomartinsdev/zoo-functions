817f35bfc5575191f74d8557d1f0b6d920871b6c
Leonardo Martins <leonardogustavo@outlook.com>
2023-03-11 17:00:12 -0300

    Req 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,4 +10,16 @@ describe('Testes da função HandlerElephants', () => {
   it('Argumento averageAge retorna número próx a 10.5', () => {
     expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
+  it('Argumento location retorna a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Argumento popularity retorna número >= 5', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Argumento availability retorna array de dias que não contém Monday', () => {
+    expect(handlerElephants('availability')).not.toContain('Monday');
+  });
+  it('Retorna undefined se não forem passados argumentos', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
 });
