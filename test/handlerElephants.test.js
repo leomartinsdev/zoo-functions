5ba3c660167a4580fc50bae3f5765cac76953455
Leonardo Martins <leonardogustavo@outlook.com>
2023-03-11 16:51:46 -0300

    Req 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Retorna número inteiro 4 ao passar argumento count', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Retorna array de nomes que possui Jefferson ao passar names', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Argumento averageAge retorna número próx a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+});
