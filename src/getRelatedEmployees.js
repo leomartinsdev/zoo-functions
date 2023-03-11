a1f14e10e76a4a7caf117370287fd3cb7601d19e
Leonardo Martins <leonardogustavo@outlook.com>
2023-03-11 16:32:59 -0300

    Req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const eGerente = data.employees
+    .some((elemento) => elemento.managers.includes(id));
+  return eGerente;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const liderados = data.employees
+      .filter((elemento) => elemento.managers.includes(managerId));
+    const arrayNomes = [];
+    liderados
+      .forEach((elemento) => arrayNomes.push(`${elemento.firstName} ${elemento.lastName}`));
+    return arrayNomes;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
