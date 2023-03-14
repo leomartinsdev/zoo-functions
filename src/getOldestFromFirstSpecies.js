12c08b02b0b8835d30bcaf4d6aea81f739be691c
Leonardo Martins <leonardogustavo@outlook.com>
2023-03-14 17:56:58 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const achaFunc = data.employees
+    .find((ele) => ele.id === id);
+  const achaAnimais = achaFunc.responsibleFor[0];
+  const achaSpecies = data.species.find((ele) => ele.id === achaAnimais).residents;
+  const achaIdades = achaSpecies.map((ele) => ele.age)
+    .sort((a, b) => b - a)[0];
+  return Object.values(achaSpecies.find((ele) => ele.age === achaIdades));
 };
 
 module.exports = getOldestFromFirstSpecies;
