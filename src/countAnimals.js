eb29c6b74de5b9d33d77c0d5a1c41cc1f33b5584
Leonardo Martins <leonardogustavo@outlook.com>
2023-03-14 12:57:13 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    const todos = {};
+    for (let i = 0; i < data.species.length; i += 1) {
+      todos[data.species[i].name] = data.species[i].residents.length;
+    }
+    return todos;
+  }
+  if (Object.keys(animal).length === 1) {
+    return data.species
+      .find((especie) => (Object.values(animal).includes(especie.name))).residents.length;
+  }
+  if (Object.keys(animal).length === 2) {
+    const arrFiltered = data.species
+      .find((especie) => (Object.values(animal).includes(especie.name))).residents
+      .filter((elemento) => JSON.stringify(elemento.sex)
+        === JSON.stringify(animal.sex));
+    return arrFiltered.length;
+  }
 };
 
 module.exports = countAnimals;
